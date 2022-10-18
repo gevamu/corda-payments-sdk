acc3c040d69ba751798a86dc4e870a680b319b0d
Nikolay Dorofeev <nikolay.dorofeev@eaxtprosystems.com>
2022-10-18 14:30:59 +0000

    Move deployNodes task to gateway

--- build-logic/src/main/kotlin/com.gevamu.cordformation-conventions.gradle.kts
+++ build-logic/src/main/kotlin/com.gevamu.cordformation-conventions.gradle.kts
@@ -1,12 +1,8 @@
-import net.corda.plugins.Cordform
-// Variable to check corda jar like in cordformation source code
-import org.gradle.api.plugins.JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME
-
 plugins {
     java
     id("net.corda.plugins.cordformation")
-    //id("net.corda.plugins.cordapp")
-    id("com.gevamu.kotlin-cordapp-conventions")
+    id("net.corda.plugins.cordapp")
+    //id("com.gevamu.kotlin-cordapp-conventions")
 }
 
 configurations {
@@ -18,30 +14,12 @@ repositories {
     maven("https://software.r3.com/artifactory/corda")
 }
 
-//dependencies{
-//    cordaProvided("net.corda:corda-node-api:4.9.3")
-//
-//    cordaRuntimeOnly("net.corda:corda:4.9.3")
-//
-//    // logging. Corda 4.9 provides log4j 2.17.1
-//    cordaProvided("org.apache.logging.log4j:log4j-api:2.17.1")
-//    cordaProvided("org.apache.logging.log4j:log4j-core:2.17.1")
-//}
+dependencies{
+    //cordaProvided("net.corda:corda-core:4.9.3")
+    cordaRuntimeOnly("net.corda:corda-node-api:4.9")
+    cordaRuntimeOnly("net.corda:corda:4.9")
 
-tasks.register<Cordform>("deployNodes") {
-    // Check corda jar like in cordformation source code
-    // FIXME: Corda JAR is not found while it exists
-    project.logger.info("check_corda_jar")
-    project.logger.info(RUNTIME_CLASSPATH_CONFIGURATION_NAME)
-    val cordaPath = project.configurations.getByName(RUNTIME_CLASSPATH_CONFIGURATION_NAME)
-    project.logger.info(cordaPath
-        .filter { it.toString().contains("\\Qcorda\\E(-enterprise)?-\\Q4.9.3\\E(-.+)?\\.jar\$".toRegex()) }
-        .joinToString{it.toString()})
-    node {
-        name("PaymentGateway")
-        p2pAddress("0.0.0.0")
-        p2pPort(10001)
-        cordapp(project(":xflows"))
-
-    }
+    // logging. Corda 4.9 provides log4j 2.17.1
+    cordaProvided("org.apache.logging.log4j:log4j-api:2.17.1")
+    cordaProvided("org.apache.logging.log4j:log4j-core:2.17.1")
 }
