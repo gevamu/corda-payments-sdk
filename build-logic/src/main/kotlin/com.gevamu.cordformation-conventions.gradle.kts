291348f658e5c2da022414f788efeac8c1e7af80
Nikolay Dorofeev <nikolay.dorofeev@eaxtprosystems.com>
2022-10-19 06:59:36 +0000

    Apply configs similar to https://github.com/corda/corda-gradle-plugins/blob/2033a456852d7b2a31fb93c16355d19d2a6ee7ab/cordformation/src/test/resources/DeployTwoNodeCordapp.gradle.kts

--- build-logic/src/main/kotlin/com.gevamu.cordformation-conventions.gradle.kts
+++ build-logic/src/main/kotlin/com.gevamu.cordformation-conventions.gradle.kts
@@ -1,13 +1,25 @@
+import net.corda.plugins.Cordform
+
 plugins {
-    java
+    //java
     id("net.corda.plugins.cordformation")
+    // Cordapp plugin provides cordaRuntineOnly() method
     id("net.corda.plugins.cordapp")
     //id("com.gevamu.kotlin-cordapp-conventions")
 }
 
 configurations {
-    cordaCordapp.get().isCanBeResolved = true
+    // Unless Cordapp can be resolved, deployNodes task can't be created
+    cordaCordapp {
+        isCanBeResolved = true
+    }
 }
+
+val corda_group: String by project
+val corda_bundle_version: String by project
+val corda_release_version: String by project
+
+val cordaCPK by configurations.creating
 
 repositories {
     mavenCentral()
@@ -18,8 +30,38 @@ dependencies{
     //cordaProvided("net.corda:corda-core:4.9.3")
     cordaRuntimeOnly("net.corda:corda-node-api:4.9")
     cordaRuntimeOnly("net.corda:corda:4.9")
+    cordapp(project(":xflows"))
 
     // logging. Corda 4.9 provides log4j 2.17.1
-    cordaProvided("org.apache.logging.log4j:log4j-api:2.17.1")
-    cordaProvided("org.apache.logging.log4j:log4j-core:2.17.1")
+//    cordaProvided("org.apache.logging.log4j:log4j-api:2.17.1")
+//    cordaProvided("org.apache.logging.log4j:log4j-core:2.17.1")
+}
+
+val cpk = tasks.register<Jar>("cpk") {
+    archiveBaseName.set("locally-built")
+    archiveClassifier.set("cordapp")
+    archiveExtension.set("cpk")
+}
+
+artifacts {
+    add("cordaCPK", cpk)
+}
+
+tasks.register<Cordform>("deployNodes") {
+    dependsOn.add("cpk")
+    // Check corda jar like in cordformation source code
+    // FIXME: Corda JAR is not found while it exists
+    project.logger.info("check_corda_jar")
+    project.logger.info(JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME)
+    val cordaPath = project.configurations.getByName(JavaPlugin.RUNTIME_CLASSPATH_CONFIGURATION_NAME)
+    project.logger.info(cordaPath
+        .filter { it.toString().contains("\\Qcorda\\E(-enterprise)?-\\Q4.9\\E(-.+)?\\.jar\$".toRegex()) }
+        .joinToString{it.toString()})
+    node {
+        name("PaymentGateway")
+        p2pAddress("0.0.0.0")
+        p2pPort(10001)
+        cordapp(project(":xflows"))
+
+    }
 }
