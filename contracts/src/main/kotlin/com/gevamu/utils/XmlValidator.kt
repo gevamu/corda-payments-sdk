ac8a9c89cc317f3c374c82facd14bc1dbfec2a0f
Nikolay Dorofeev <nikolay.dorofeev@eaxtprosystems.com>
2022-10-24 12:14:59 +0000

    Check request payload

--- contracts/src/main/kotlin/com/gevamu/utils/XmlValidator.kt
+++ contracts/src/main/kotlin/com/gevamu/utils/XmlValidator.kt
@@ -1,17 +1,36 @@
 package com.gevamu.utils
 
+import net.corda.core.serialization.serialize
+import org.w3c.dom.Document
+import org.xml.sax.InputSource
 import org.xml.sax.SAXException
 import java.io.File
 import java.io.StringReader
-import java.nio.file.Path
-import java.nio.file.Paths
+import javax.xml.parsers.DocumentBuilder
+import javax.xml.parsers.DocumentBuilderFactory
 import javax.xml.transform.stream.StreamSource
 import javax.xml.validation.SchemaFactory
 
 
 class XmlValidator {
     companion object {
-        fun validate(xmlObject: String): Boolean {
+        @Throws(Exception::class)
+        private fun loadXMLFromString(xml: String): Document {
+            val factory: DocumentBuilderFactory = DocumentBuilderFactory.newInstance()
+            val builder: DocumentBuilder = factory.newDocumentBuilder()
+            val inputSource = InputSource(StringReader(xml))
+            return builder.parse(inputSource)
+        }
+        private fun getRequestPayload(xmlRequest: String): String{
+//            val request: Document = loadXMLFromString(xmlRequest)
+//            val payload = request.getElementsByTagName("RequestPayload")
+//            println(payload.serialize().toString())
+//            return payload.serialize().toString()
+            return xmlRequest
+                .substringAfter("<RequestPayload>")
+                .substringBefore("</RequestPayload>")
+        }
+        fun validate(xmlRequest: String): Boolean {
             println("Validation Starts now!")
             //println(Paths.get(XmlValidator::class.java.getResource("").toURI()).toUri())
             //val mySchemaPath: Path = Paths.get(XmlValidator::class.java.getResource("xsd/pain.001.001.11.xsd").toURI())
@@ -23,7 +42,7 @@ class XmlValidator {
                 .newSchema(schemaLocation)
                 .newValidator()
 
-            val source = StreamSource(StringReader(xmlObject))
+            val source = StreamSource(StringReader(getRequestPayload(xmlRequest)))
 
             val start = System.currentTimeMillis()
             try {
