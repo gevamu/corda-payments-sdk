a49140179fccda02355720bb216a50af639ca8ed
Nikolay Dorofeev <nikolay.dorofeev@eaxtprosystems.com>
2022-10-25 09:15:45 +0000

    Refactor XmlValidator

--- contracts/src/main/kotlin/com/gevamu/utils/XmlValidator.kt
+++ contracts/src/main/kotlin/com/gevamu/utils/XmlValidator.kt
@@ -2,26 +2,31 @@ package com.gevamu.utils
 
 import com.gevamu.xsd.XsdRepository
 import java.io.StringReader
+import javax.xml.transform.Source
 import javax.xml.transform.stream.StreamSource
 import javax.xml.validation.SchemaFactory
+import javax.xml.validation.Validator
 
 
 class XmlValidator {
     companion object {
-        private fun getRequestPayload(xmlRequest: String): String{
+        private val pain_001_001_11Validator: Validator = createValidator(XsdRepository.getPain_001_001_11())
+
+        private fun createValidator(schemaSource: Source): Validator{
+            return SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema")
+                .newSchema(schemaSource)
+                .newValidator()
+        }
+        private fun getRequestPayload(xmlRequest: String, xmlns: String): String{
             val payload = xmlRequest
                 .substringAfter("<RequestPayload>")
                 .substringBefore("</RequestPayload>")
-            return "<Document xmlns=\"urn:iso:std:iso:20022:tech:xsd:pain.001.001.11\">$payload</Document>"
+            return "<Document xmlns=\"$xmlns\">$payload</Document>"
         }
         fun validatePain_001_001_11(xmlRequest: String) {
-            val source = StreamSource(StringReader(getRequestPayload(xmlRequest)))
-            SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema")
-                .newSchema(XsdRepository.getPain_001_001_11())
-                .newValidator()
-                .validate(source)
-
-            println("XML Request is valid")
+            val payload = getRequestPayload(xmlRequest, "urn:iso:std:iso:20022:tech:xsd:pain.001.001.11")
+            val source = StreamSource(StringReader(payload))
+            pain_001_001_11Validator.validate(source)
         }
     }
 }
