fb5813b12f3d482c3e0844c68c5e36249d099c5b
Nikolay Dorofeev <nikolay.dorofeev@eaxtprosystems.com>
2022-10-25 13:14:23 +0000

    Initiate schema, not validator

--- contracts/src/main/kotlin/com/gevamu/iso20022/schema/Validator.kt
+++ contracts/src/main/kotlin/com/gevamu/iso20022/schema/Validator.kt
@@ -5,18 +5,18 @@ import org.xml.sax.SAXException
 import java.io.StringReader
 import javax.xml.transform.Source
 import javax.xml.transform.stream.StreamSource
+import javax.xml.validation.Schema
 import javax.xml.validation.SchemaFactory
 import kotlin.jvm.Throws
 
 class Validator {
     companion object {
-        private val customerCreditTransferInitiationSchemaValidator: javax.xml.validation.Validator =
-            createValidator(Repository.getCustomerCreditTransferInitiationSchema())
+        private val customerCreditTransferInitiationSchema: Schema =
+            createSchema(Repository.getCustomerCreditTransferInitiationSchema())
 
-        private fun createValidator(schemaSource: Source): javax.xml.validation.Validator {
+        private fun createSchema(schemaSource: Source): Schema {
             return SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema")
                 .newSchema(schemaSource)
-                .newValidator()
         }
 
         @Throws(SAXException::class)
@@ -26,7 +26,7 @@ class Validator {
                 .substringBefore("</RequestPayload>")
             val xmlToValidate = "<Document xmlns=\"urn:iso:std:iso:20022:tech:xsd:pain.001.001.11\">$payload</Document>"
             val source = StreamSource(StringReader(xmlToValidate))
-            customerCreditTransferInitiationSchemaValidator.validate(source)
+            customerCreditTransferInitiationSchema.newValidator().validate(source)
         }
     }
 }
