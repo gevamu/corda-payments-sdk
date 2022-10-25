09282206a5a464997277beacd6269e4e47cbe4f6
Nikolay Dorofeev <nikolay.dorofeev@eaxtprosystems.com>
2022-10-25 13:28:23 +0000

    Convert XmlValidator to object

--- contracts/src/main/kotlin/com/gevamu/iso20022/schema/XmlValidator.kt
+++ contracts/src/main/kotlin/com/gevamu/iso20022/schema/XmlValidator.kt
@@ -8,24 +8,22 @@ import javax.xml.validation.Schema
 import javax.xml.validation.SchemaFactory
 import kotlin.jvm.Throws
 
-class XmlValidator {
-    companion object {
-        private val customerCreditTransferInitiationSchema: Schema =
-            createSchema(SchemaRepository.getCustomerCreditTransferInitiationSchema())
+object XmlValidator {
+    private val customerCreditTransferInitiationSchema: Schema =
+        createSchema(SchemaRepository.getCustomerCreditTransferInitiationSchema())
 
-        private fun createSchema(schemaSource: Source): Schema {
-            return SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema")
-                .newSchema(schemaSource)
-        }
+    private fun createSchema(schemaSource: Source): Schema {
+        return SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema")
+            .newSchema(schemaSource)
+    }
 
-        @Throws(SAXException::class)
-        fun validateCustomerCreditTransferInitiationRequest(xmlRequest: String) {
-            val payload = xmlRequest
-                .substringAfter("<RequestPayload>")
-                .substringBefore("</RequestPayload>")
-            val xmlToValidate = "<Document xmlns=\"urn:iso:std:iso:20022:tech:xsd:pain.001.001.11\">$payload</Document>"
-            val source = StreamSource(StringReader(xmlToValidate))
-            customerCreditTransferInitiationSchema.newValidator().validate(source)
-        }
+    @Throws(SAXException::class)
+    fun validateCustomerCreditTransferInitiationRequest(xmlRequest: String) {
+        val payload = xmlRequest
+            .substringAfter("<RequestPayload>")
+            .substringBefore("</RequestPayload>")
+        val xmlToValidate = "<Document xmlns=\"urn:iso:std:iso:20022:tech:xsd:pain.001.001.11\">$payload</Document>"
+        val source = StreamSource(StringReader(xmlToValidate))
+        customerCreditTransferInitiationSchema.newValidator().validate(source)
     }
 }
