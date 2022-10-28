b054e983594b8782af84201429ed6dcd14df3075
Anton Sitnikov <anton.sitnikov@exactprosystems.com>
2022-10-28 05:57:36 +0000

    Add lifecycle tasks to root project

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,24 @@
 plugins {
+    // Add lifecycle tasks
+    base
     // Kotlin plugin should be loaded only once
     id("com.gevamu.kotlin-common-conventions") apply false
 }
 
 group = "com.gevamu"
+
+tasks.clean {
+    dependsOn("contracts:$name", "workflows:$name")
+}
+
+tasks.check {
+    dependsOn("contracts:$name", "workflows:$name")
+}
+
+tasks.assemble {
+    dependsOn("contracts:$name", "workflows:$name")
+}
+
+tasks.build {
+    dependsOn("contracts:$name", "workflows:$name")
+}
