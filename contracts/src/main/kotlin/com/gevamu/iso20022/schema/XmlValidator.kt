db4e6542e9c9a04b5981113bb47c7e1877ea9009
nikita.shaposhnikov <nikita.shaposhnikov@exactprosystems.com>
2022-11-08 15:57:56 +0000

    xml validation without document tag

--- contracts/src/main/kotlin/com/gevamu/iso20022/schema/XmlValidator.kt
+++ contracts/src/main/kotlin/com/gevamu/iso20022/schema/XmlValidator.kt
@@ -1,16 +1,21 @@
 package com.gevamu.iso20022.schema
 
-import org.xml.sax.SAXException
-import java.io.StringReader
+import javax.xml.parsers.SAXParserFactory
 import javax.xml.transform.Source
-import javax.xml.transform.stream.StreamSource
 import javax.xml.validation.Schema
 import javax.xml.validation.SchemaFactory
+import org.xml.sax.SAXException
+import org.xml.sax.XMLReader
 import kotlin.jvm.Throws
 
 object XmlValidator {
     private val customerCreditTransferInitiationSchema: Schema =
         createSchema(SchemaRepository.getCustomerCreditTransferInitiationSchema())
+
+    private val xmlReader: XMLReader = SAXParserFactory.newInstance().apply {
+        isValidating = false
+        schema = customerCreditTransferInitiationSchema
+    }.newSAXParser().xmlReader
 
     private fun createSchema(schemaSource: Source): Schema {
         return SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema")
@@ -19,9 +24,6 @@ object XmlValidator {
 
     @Throws(SAXException::class)
     fun validateCustomerCreditTransferInitiationRequest(xmlRequestPayload: String) {
-        val xmlToValidate =
-            "<Document xmlns=\"urn:iso:std:iso:20022:tech:xsd:pain.001.001.11\">$xmlRequestPayload</Document>"
-        val sourceToValidate = StreamSource(StringReader(xmlToValidate))
-        customerCreditTransferInitiationSchema.newValidator().validate(sourceToValidate)
+        xmlReader.parse(xmlRequestPayload)
     }
 }
