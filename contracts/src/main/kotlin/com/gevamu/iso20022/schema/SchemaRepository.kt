9b8fdd48bf890abdd92cfb4b4731a8565161242f
nikita.shaposhnikov <nikita.shaposhnikov@exactprosystems.com>
2022-11-15 14:00:47 +0000

    SchemaRepository.kt refactoring

--- contracts/src/main/kotlin/com/gevamu/iso20022/schema/SchemaRepository.kt
+++ contracts/src/main/kotlin/com/gevamu/iso20022/schema/SchemaRepository.kt
@@ -4,10 +4,15 @@ import com.gevamu.iso20022.pain.Document
 import javax.xml.transform.stream.StreamSource
 import kotlin.jvm.Throws
 import java.io.IOException
+import javax.xml.XMLConstants
 import javax.xml.bind.annotation.XmlSchema
+import javax.xml.validation.Schema
+import javax.xml.validation.SchemaFactory
 
 object SchemaRepository {
     val DEFAULT_NAMESPACE by lazy { Document::class.java.`package`.getDeclaredAnnotation(XmlSchema::class.java).namespace }
+
+    private val schemaFactory by lazy {  SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI) }
 
     @Throws(IOException::class)
     private fun getSource(fileName: String): StreamSource {
@@ -19,8 +24,13 @@ object SchemaRepository {
         }
     }
 
-    fun getCustomerCreditTransferInitiationSchema(): StreamSource {
-        return getSource("pain.001.001.09.xsd")
+    fun getCustomerCreditTransferInitiationSchema(): Schema {
+        val source = getSource("pain.001.001.09.xsd")
+        try {
+            return schemaFactory.newSchema(source)
+        } finally {
+            source.inputStream.close()
+        }
     }
 
 
