91ea5c2eb37622aa29b447a72554406251597e87
Nikolay Dorofeev <nikolay.dorofeev@eaxtprosystems.com>
2022-12-07 08:10:15 +0000

    Docs for PaymentFlow.kt

--- workflows/src/main/kotlin/com/gevamu/flows/PaymentFlow.kt
+++ workflows/src/main/kotlin/com/gevamu/flows/PaymentFlow.kt
@@ -15,23 +15,65 @@ import net.corda.core.identity.Party
 import net.corda.core.serialization.CordaSerializable
 import net.corda.core.transactions.TransactionBuilder
 
+/**
+ * Combinations of XSD documents describing payments and encoding types (for transformation to ByteArray)
+ *
+ * @see ByteArray
+ */
 @CordaSerializable
 enum class PaymentInstructionFormat {
+    /**
+     * pain.001.001.09.xsd + utf-8
+     *
+     * @see <a href="https://www.iso20022.org/catalogue-messages/iso-20022-messages-archive?search=Payments%20Initiation">
+     *     Payments Initiation V10
+     *     </a>
+     */
     ISO20022_V9_XML_UTF8
 }
 
+// Inline link {@link ByteArray byte array} is not rendered in IntelliJ Idea :(
+/**
+ * Dataclass describing payment instruction
+ *
+ * @param format Combination of XSD document describing the structure of payment instruction and encoding type
+ * @see PaymentInstructionFormat
+ *
+ * @param paymentInstruction XML document created due to specified XSD
+ * and cast to ByteArray in specified encoding
+ */
 @CordaSerializable
 class PaymentInstruction(
     val format: PaymentInstructionFormat,
     val paymentInstruction: ByteArray
 )
 
+/**
+ * Corda flow for payment initiation
+ *
+ * @param paymentInstruction Instruction for the payment, you want to execute
+ * @see PaymentInstruction
+ *
+ * @param gateway Identification of the Gevamu Gateway Corda node to process payment
+ * @see Party
+ *
+ * @param uniquePaymentId Identification of the new payment; generated automatically by default
+ *
+ * @return List with single created payment state
+ * @see Payment
+ */
 @StartableByRPC
 class PaymentFlow(
-    private val paymentInstruction: PaymentInstruction, private val gateway: Party,
+    private val paymentInstruction: PaymentInstruction,
+    private val gateway: Party,
     private val uniquePaymentId: UUID = UUID.randomUUID(),
 ) : FlowLogic<List<StateAndRef<Payment>>>() {
 
+    /**
+     * Start main flow logic
+     *
+     * @return List of created payment states
+     */
     @Suspendable
     override fun call(): List<StateAndRef<Payment>> {
         val xmlService = serviceHub.cordaService(XmlService::class.java)
