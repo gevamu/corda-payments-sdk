72837f2f164c714e648057c5de054c2e6a9530c6
Nikolay Dorofeev <dorich2000@gmail.com>
2022-12-21 10:32:09 +0400

    Generate api reference with dokka

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,21 @@ plugins {
     base
     // Kotlin plugin should be loaded only once
     id("com.gevamu.kotlin-common-conventions") apply false
+    // Generate api reference with dokka
+    id("org.jetbrains.dokka") version "1.7.20"
+}
+
+// Apply dokka to subprojects and dokka dependencies
+allprojects {
+    repositories {
+        mavenCentral()
+    }
+
+    apply(plugin = "org.jetbrains.dokka")
+
+    dependencies {
+        dokkaHtmlPlugin("org.jetbrains.dokka:kotlin-as-java-plugin:1.7.20")
+    }
 }
 
 group = "com.gevamu.corda"
