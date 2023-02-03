90046940a23ee323309e8906b9b838ddf2a616d5
nikita.shaposhnikov <nikita.shaposhnikov@exactprosystems.com>
2023-02-03 15:32:30 +0000

    move iso-pain classes to different project add .cache to gitignore in web-ui xsl script to transform xml

---
+++ corda-iso/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    // Include Kotlin Common Conventions
+    id("com.gevamu.kotlin-common-conventions")
+    // Apply common settings for cordapps.
+    id("com.gevamu.kotlin-cordapp-conventions")
+}
+
+group = rootProject.group
